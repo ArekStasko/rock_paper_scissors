561b7f27f78928bc40374fa15ccc052fc09a4abc
ArekStasko <arek.stasko@gmail.com>
2021-09-14 05:37:26 -0700

    fix lack of one conditional

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
+print('Welcome to Rock Paper Scissors game !')
 player_1_choice = input('[ enter Player 1\'s choice ]: ').lower()
 player_2_choice = input('[ enter Player 2\'s choice ]: ').lower()
 
+
+
 if player_1_choice == player_2_choice:
     print('Draw !')
-elif (player_1_choice == 'rock') and player_2_choice != 'paper':
+elif player_1_choice == 'rock' and player_2_choice != 'paper':
     print('Player1 wins !')
-elif (player_1_choice == 'scissors') and player_2_choice == 'paper':
+elif player_1_choice == 'scissors' and player_2_choice == 'paper':
+    print('Player1 wins !')
+elif player_1_choice == 'paper' and player_2_choice == 'rock':
     print('Player1 wins !')
 else:
     print('Player2 wins !')
